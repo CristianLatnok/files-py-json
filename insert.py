a2642dfef12b81b887d4cb7f0caf382fbe52b145
CristianLatnok <34199780+CristianLatnok@users.noreply.github.com>
2024-01-17 00:18:11 -0500

    Update Insert File

--- insert.py
+++ insert.py
@@ -83,4 +83,4 @@ def ejecutar_consulta():
         return jsonify({'error: no puede mostrar resultado': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
